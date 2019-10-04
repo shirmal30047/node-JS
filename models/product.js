46c59c688bd17a93d5d0faf98f52e802a6f60d3b
Shirmal Seneviratne <ShirmalS@inbaycmb.local>
2019-10-04 09:39:00 +0530

    created all CRUD oprations for product

--- models/product.js
+++ models/product.js
@@ -2,20 +2,26 @@ const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
-      .then(result => {
-      })
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
+      .then(result => {})
       .catch(err => {
         console.log(err);
       });
@@ -43,6 +49,19 @@ class Product {
       .next()
       .then(product => {
         return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(result => {
+        console.log("deleting product");
       })
       .catch(err => {
         console.log(err);
--- routes/admin.js
+++ routes/admin.js
@@ -15,10 +15,10 @@ router.get('/products',adminController.getProducts);
 
 router.post('/add-product',adminController.postAddProducts);
 
-// router.get('/edit-product/:productId',adminController.getEditProduct);
+router.get('/edit-product/:productId',adminController.getEditProduct);
 
-// router.post('/edit-product',adminController.postEditProduct);
+router.post('/edit-product',adminController.postEditProduct);
 
-// router.post('/delete-product',adminController.postDeleteProduct);
+router.post('/delete-product',adminController.postDeleteProduct);
 
 module.exports = router;
